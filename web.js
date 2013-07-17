27856ba8fb85ac1df736e070cea6f53d7730d7e2
Behzad Ardakani <behzad@ardakani.se>
2013-07-17 19:40:29 +0200

    fixed images

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var encoding ='utf-8'
+app.use(express.static(__dirname + '/'));
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html',encoding);
   response.send(buffer.toString(encoding));
